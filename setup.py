84c60171bf5fad332bb82ff53233aee36cf18bfb
Tanguy Esteoule <tanguy.esteoule@meteoswift.com>
2018-12-10 12:21:27 +0100

    add module structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pamak',
+      version='0.1',
+      description='A python framework for developing multi-agent systems',
+      url='https://github.com/tanguyesteoule/pamak',
+      author='Tanguy Esteoule',
+      author_email='tanguy.esteoule@gmail.com',
+      license='MIT',
+      packages=['pamak'],
+      zip_safe=False)
